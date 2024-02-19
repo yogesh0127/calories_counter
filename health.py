e72eb23402766b3d1d832373886889b62773da01
yogesh0127 <78494708+yogesh0127@users.noreply.github.com>
2024-02-19 23:54:54 +0530

    Update health.py

--- health.py
+++ health.py
@@ -14,7 +14,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_repsonse(input,image,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,image[0],prompt])
+    response=model.generate_content([image[0],input,prompt])
     return response.text
 
 def input_image_setup(uploaded_file):
